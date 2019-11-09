2a531612e7fa807bc1f64c3c2c37df322328f026
Łukasz Gajowy <lukasz.gajowy@gmail.com>
2019-11-09 19:57:52 +0100

    Add recursive solution along with tests.

---
+++ src/main/groovy/com/lgajowy/treasurehunt/TreasureHunt.groovy
@@ -0,0 +1,54 @@
+package com.lgajowy.treasurehunt
+
+import groovy.transform.CompileStatic
+
+/**
+ * Solution #1: uses native data structures and functional concepts (optional, tail recursion).
+ * Assumptions:
+ *  - There are max 9 rows and 9 columns
+ *  - Game field's values are hints: first digit points next row and second digit points to next column.
+ *  - we rely only on hints provided in the game field. If hints do not lead to the treasure,
+ *  it will not be found.
+ */
+@CompileStatic
+class TreasureHunt {
+
+    private final int[][] gameField
+
+    private final int segmentCount
+
+    TreasureHunt(int[][] gameField) {
+        this.gameField = gameField
+        this.segmentCount = gameField.length * gameField[0].length
+    }
+
+    /**
+     * Finds treasure in a given game field.
+     *
+     * @param startRow - row position value from 1 to gameField.length (inclusive)
+     * @param startColumn - column position value from 1 to gameField[0].length (inclusive)
+     * @return Optional of list of steps to find the treasure or empty() if treasure could not be found.
+     */
+    Optional<List<Integer>> findTreasure(int startRow, int startColumn) {
+        return lookForTreasure(startRow - 1, startColumn - 1, [], 0)
+    }
+
+    private Optional<List<Integer>> lookForTreasure(int row, int column, List<Integer> stepsSoFar, int stepCount) {
+        if (stepCount > segmentCount) {
+            return Optional.empty()
+        }
+
+        Integer newStep = "${row + 1}${column + 1}".toInteger()
+        stepsSoFar.add(newStep)
+
+        if ("${row + 1}${column + 1}" == "${gameField[row][column]}") {
+            return Optional.of(stepsSoFar)
+        } else {
+            int value = gameField[row][column]
+            int newRow = (value / 10) - 1 as Integer
+            int newCol = (value % 10) - 1
+
+            return lookForTreasure(newRow, newCol, stepsSoFar, stepCount + 1)
+        }
+    }
+}
---
+++ src/test/groovy/com/lgajowy/treasurehunt/TreasureHuntTest.groovy
@@ -0,0 +1,59 @@
+package com.lgajowy.treasurehunt
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class TreasureHuntTest extends Specification {
+
+    private final int[][] TEST_GAME_FIELD = [[55, 14, 25, 52, 21],
+                                             [44, 31, 11, 53, 43],
+                                             [24, 13, 45, 12, 34],
+                                             [42, 22, 43, 32, 41],
+                                             [51, 23, 33, 54, 15]]
+
+    @Unroll
+    def "should show proper path to treasure when starting from row #row and column #column"(int row, int column, List<Integer> steps) {
+        given:
+        TreasureHunt treasureHunt = new TreasureHunt(TEST_GAME_FIELD)
+
+        expect:
+        treasureHunt.findTreasure(row, column) == Optional.of(steps)
+
+        where:
+        row | column | steps
+        1   | 1      | [11, 55, 15, 21, 44, 32, 13, 25, 43]
+        1   | 2      | [12, 14, 52, 23, 11, 55, 15, 21, 44, 32, 13, 25, 43]
+        5   | 1      | [51]
+        5   | 5      | [55, 15, 21, 44, 32, 13, 25, 43]
+        1   | 5      | [15, 21, 44, 32, 13, 25, 43]
+    }
+
+    def "should show proper path for smaller array as well"() {
+        given:
+        int[][] gameFieldWithoutTheTreasure = [[11, 11],
+                                               [12, 12]]
+        TreasureHunt treasureHunt = new TreasureHunt(gameFieldWithoutTheTreasure)
+
+        when:
+        Optional<List<Integer>> stepsToFindTheTreasure = treasureHunt.findTreasure(2, 2)
+
+        then:
+        Optional.of([22, 12, 11]) == stepsToFindTheTreasure
+    }
+
+    def "should not find the treasure when it's not there"() {
+        given:
+        int[][] gameFieldWithoutTheTreasure = [[55, 14, 25, 52, 21],
+                                               [44, 31, 11, 53, 43],
+                                               [24, 13, 45, 12, 34],
+                                               [42, 22, 44, 32, 41],
+                                               [55, 23, 33, 54, 15]]
+        TreasureHunt treasureHunt = new TreasureHunt(gameFieldWithoutTheTreasure)
+
+        when:
+        Optional<List<Integer>> stepsToFindTheTreasure = treasureHunt.findTreasure(1, 1)
+
+        then:
+        Optional.empty() == stepsToFindTheTreasure
+    }
+}
